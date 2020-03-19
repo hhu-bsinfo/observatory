ad25faac066a57ad3e0021e995cead24170aa180
Fabian Ruhland <Fabian.Ruhland@uni-duesseldorf.de>
2020-03-19 12:11:03 +0100

    java: Wait 1s instead of 100ms after a failed connection attempt

--- java/benchmark/src/main/java/de/hhu/bsinfo/observatory/benchmark/Benchmark.java
+++ java/benchmark/src/main/java/de/hhu/bsinfo/observatory/benchmark/Benchmark.java
@@ -211,7 +211,7 @@ public abstract class Benchmark {
                         attempt++;
 
                         try {
-                            Thread.sleep(100);
+                            Thread.sleep(1000);
                         } catch (InterruptedException ignored) {}
                     } else {
                         LOGGER.error("Setting up off channel communication failed", e);
